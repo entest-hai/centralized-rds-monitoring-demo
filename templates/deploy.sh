3ad67ee4df38025d0b9cb795d9a45d3708c3a6aa
haitran <hai@entest.io>
2024-08-25 06:41:09 +0000

    first

---
+++ templates/deploy.sh
@@ -0,0 +1,21 @@
+aws cloudformation create-stack \
+ --stack-name network-stack \
+ --template-body file://1-network.yaml \
+ --capabilities CAPABILITY_NAMED_IAM
+
+aws cloudformation update-stack \
+ --stack-name network-stack \
+ --template-body file://1-network.yaml \
+ --capabilities CAPABILITY_NAMED_IAM
+
+aws cloudformation create-stack \
+ --stack-name rds-instance-stack \
+ --template-body file://2-rds-instance.yaml \
+ --capabilities CAPABILITY_NAMED_IAM \
+ --parameters '[{"ParameterKey":"DBUsername","ParameterValue":"admin"},{"ParameterKey":"DBPassword","ParameterValue":"Admin2024"}]'
+
+aws cloudformation create-stack \
+ --stack-name rds-cluster-stack \
+ --template-body file://3-rds-cluster.yaml \
+ --capabilities CAPABILITY_NAMED_IAM \
+ --parameters '[{"ParameterKey":"DBUsername","ParameterValue":"admin"},{"ParameterKey":"DBPassword","ParameterValue":"Admin2024"}]'
